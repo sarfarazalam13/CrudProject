04c6e606e509e2a602ee6bb6efe09d17fcec511c
Sarfaraz <alamsarfaraz386@gmail.com>
2025-11-18 01:27:13 +0530

    Learnt about Lombok so you dont have to make constructor Getter and Setters

---
+++ CrudProject/src/main/java/com/Proj/CrudProject/Model/CloudVendor.java
@@ -0,0 +1,16 @@
+package com.Proj.CrudProject.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class CloudVendor {
+    String vendorId;
+    String vendorName;
+    String vendorAddress;
+    String vendorPhoneNumber;
+
+}
